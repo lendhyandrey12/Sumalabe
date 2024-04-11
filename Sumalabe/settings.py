3ff5665ea4fd78e52b7497d6c2a93402c168c217
jonnienyork <delacruz.jl.bsinfotech@gmail.com>
2024-04-11 12:11:54 +0800

    Second Commit

--- Sumalabe/settings.py
+++ Sumalabe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Lendhy',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Sumalabe.urls'
+ROOT_URLCONF = 'Lendhy.urls'
 
 TEMPLATES = [
     {
